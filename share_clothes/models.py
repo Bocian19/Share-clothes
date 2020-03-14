a9363d29871fe3ffd027ffbb18d3a333c5b08ae1
Łukasz <boclukasz@gmail.com>
2020-03-14 09:54:43 +0100

    Model institution created

--- share_clothes/models.py
+++ share_clothes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, null=False)
+
+
+class Institution(models.Model):
+    FUNDACJA = 'Fun'
+    ORGANIZACJA_POZARZADOWA = 'Org'
+    ZBIORKA_LOKALNA = 'Zb'
+
+    TYPE_CHOICES = [
+        (FUNDACJA, "Fundacja"),
+        (ORGANIZACJA_POZARZADOWA, "Organizacja pozarządowa"),
+        (ZBIORKA_LOKALNA, "Zbiórka lokalna")]
+
+    name = models.CharField(max_length=64, null=False)
+    description = models.TextField(null=True)
+    type = models.CharField(choices=TYPE_CHOICES, default=FUNDACJA, max_length=3)
+
